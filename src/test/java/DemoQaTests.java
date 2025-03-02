ff1d5d387f2cd7bd9c27a6838be31390999192da
vladislav-andriushchenko <vladitheboy@gmail.com>
2025-03-02 08:44:19 -0800

    done refactoring and added assertions

--- src/test/java/DemoQaTests.java
+++ src/test/java/DemoQaTests.java
@@ -2,31 +2,45 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class DemoQaTests {
+
+    String firstName = "Jack";
+    String lastName = "Black";
+    String email = "testmail@test.com";
+    String genderRadioValue = "Male";
+    String phone = "7909654321";
+    String month = "May";
+    String day = "15";
+    String year = "1995";
+    String subject = "Music";
+    String address = "Some address";
+    String city = "Karnal";
+    String state = "Haryana";
+    String hobby = "Music";
+    String imageName = "sample-image.jpg";
 
     @BeforeAll
     static void setup() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
     }
 
     @Test
     void successfulMaxFieldsTest() {
         open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
         $("#firstName").setValue("Jack");
         $("#lastName").setValue("Black");
         $("#userEmail").setValue("testmail@test.com");
         $("#gender-radio-1").parent().$(byText("Male")).click();
-        $("#userNumber").setValue("79096543210");
+        $("#userNumber").setValue("7909654321");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("May");
         $(".react-datepicker__year-select").selectOption("1995");
@@ -38,10 +52,20 @@ public class DemoQaTests {
         $(byText("Haryana")).click();
         $("#city").click();
         $(byText("Karnal")).click();
-        $("#uploadPicture").uploadFile(new File("src/test/resources/sample-image.jpg"));
+        $("#uploadPicture").uploadFromClasspath(imageName);
         $("#submit").click();
 
         $(".modal-content").shouldBe(visible);
         $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-content").shouldHave(text(firstName + " " + lastName));
+        $(".modal-content").shouldHave(text(email));
+        $(".modal-content").shouldHave(text(genderRadioValue));
+        $(".modal-content").shouldHave(text(phone));
+        $(".modal-content").shouldHave(text(day + " " + month + "," + year));
+        $(".modal-content").shouldHave(text(subject));
+        $(".modal-content").shouldHave(text(hobby));
+        $(".modal-content").shouldHave(text(imageName));
+        $(".modal-content").shouldHave(text(address));
+        $(".modal-content").shouldHave(text(state + " " + city));
     }
 }
