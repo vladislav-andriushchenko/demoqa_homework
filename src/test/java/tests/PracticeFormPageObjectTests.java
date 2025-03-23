ba057c363c9212fd0b080c7b35b9717373cdf682
vladislav-andriushchenko <vladitheboy@gmail.com>
2025-03-23 15:39:31 +0400

    added testData

--- src/test/java/tests/PracticeFormPageObjectTests.java
+++ src/test/java/tests/PracticeFormPageObjectTests.java
@@ -4,24 +4,28 @@ import org.junit.jupiter.api.Test;
 import pages.PracticeFormPage;
 import pages.components.DialogWindow;
 
+import static utils.TestData.*;
+
 public class PracticeFormPageObjectTests extends TestBase {
 
     PracticeFormPage practiceFormPage = new PracticeFormPage();
     DialogWindow dialogWindow = new DialogWindow();
-    String firstName = "Jack";
-    String lastName = "Black";
-    String email = "testmail@test.com";
-    String genderRadioValue = "Male";
-    String phone = "7909654321";
-    String month = "May";
-    String day = "15";
-    String year = "1995";
-    String subject = "Maths";
-    String address = "Some address";
-    String city = "Karnal";
-    String state = "Haryana";
-    String hobby = "Music";
-    String imageName = "sample-image.jpg";
+
+    String firstName = getRandomFirstName(),
+            lastName = getRandomLastName(),
+            email = getRandomEmail(),
+            phone = getRandomPhoneNumber(),
+            genderRadioValue = getRandomGender(),
+            address = getRandomAddress(),
+            imageName = getRandomFile(),
+            hobby = getRandomHobby(),
+            subject = getRandomSubject(),
+            state = getRandomState(),
+            city = getRandomCity(state),
+            month = getRandomMonth(),
+            year = getRandomYear(),
+            day = getRandomDay();
+
 
     @Test
     void successfulMaxFieldsTest() {
@@ -47,14 +51,14 @@ public class PracticeFormPageObjectTests extends TestBase {
                 .checkModalTitle("Thanks for submitting the form")
                 .checkResult(firstName + " " + lastName)
                 .checkResult(email)
-                .checkResult( genderRadioValue)
-                .checkResult( phone)
-                .checkResult( day + " " + month + "," + year)
-                .checkResult( subject)
-                .checkResult( hobby)
-                .checkResult( imageName)
-                .checkResult( address)
-                .checkResult( state + " " + city);
+                .checkResult(genderRadioValue)
+                .checkResult(phone)
+                .checkResult(day + " " + month + "," + year)
+                .checkResult(subject)
+                .checkResult(hobby)
+                .checkResult(imageName)
+                .checkResult(address)
+                .checkResult(state + " " + city);
     }
 
     @Test
@@ -72,8 +76,8 @@ public class PracticeFormPageObjectTests extends TestBase {
         practiceFormPage
                 .checkModalTitle("Thanks for submitting the form")
                 .checkResult(firstName + " " + lastName)
-                .checkResult( genderRadioValue)
-                .checkResult( phone);
+                .checkResult(genderRadioValue)
+                .checkResult(phone);
     }
 
     @Test
---
+++ src/test/java/utils/TestData.java
@@ -0,0 +1,90 @@
+package utils;
+
+import com.github.javafaker.Faker;
+
+import java.io.File;
+import java.util.*;
+
+import static java.util.Objects.requireNonNull;
+
+
+public class TestData {
+
+    static Faker faker = new Faker();
+    static final String dir = "src/test/resources/images";
+
+    static Map<String, List<String>> stateCityMap = new HashMap<>() {{
+        put("NCR", Arrays.asList("Delhi", "Gurgaon", "Noida"));
+        put("Uttar Pradesh", Arrays.asList("Agra", "Lucknow", "Merrut"));
+        put("Haryana", Arrays.asList("Karnal", "Panipat"));
+        put("Rajasthan", Arrays.asList("Jaipur", "Jaiselmer"));
+    }};
+
+    static ArrayList<String>
+            gender = new ArrayList<>(Arrays.asList("Male", "Female", "Other")),
+            hobbies = new ArrayList<>(Arrays.asList("Sports", "Reading", "Music")),
+            subjects = new ArrayList<>(Arrays.asList("Maths", "Arts", "History", "Biology", "Economics"));
+
+    public static String getRandomState() {
+        List<String> states = new ArrayList<>(stateCityMap.keySet());
+        return states.get(faker.random().nextInt(states.size()));
+    }
+
+    public static String getRandomCity(String state) {
+        List<String> cities = stateCityMap.get(state);
+        return cities.get(faker.random().nextInt(cities.size()));
+    }
+
+    public static String getRandomFirstName() {
+        return faker.name().firstName();
+    }
+
+    public static String getRandomLastName() {
+        return faker.name().lastName();
+    }
+
+    public static String getRandomEmail() {
+        return faker.internet().emailAddress();
+    }
+
+    public static String getRandomPhoneNumber() {
+        return faker.phoneNumber().subscriberNumber(10);
+    }
+
+    public static String getRandomAddress() {
+        return faker.address().fullAddress();
+    }
+
+    public static String getRandomMonth() {
+        return faker.options().option(
+                "January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
+        );
+    }
+
+    public static String getRandomDay() {
+        return String.format("%03d", faker.number().numberBetween(1, 31));
+    }
+
+    public static String getRandomYear() {
+        return String.valueOf(faker.number().numberBetween(1900, 2100));
+    }
+
+    public static String getRandomGender() {
+        return gender.get(faker.random().nextInt(gender.size()));
+    }
+
+    public static String getRandomHobby() {
+        return hobbies.get(faker.random().nextInt(hobbies.size()));
+    }
+
+    public static String getRandomSubject() {
+        return subjects.get(faker.random().nextInt(subjects.size()));
+    }
+
+    public static String getRandomFile() {
+        var fileNames = new File(dir).list();
+        var index = faker.random().nextInt(requireNonNull(fileNames).length);
+        return fileNames[index];
+    }
+}
