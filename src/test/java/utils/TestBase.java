fdf8cf25b7a4b297fa24ec7c95ea52d6179d0362
vladislav-andriushchenko <vladitheboy@gmail.com>
2025-04-20 00:39:11 +0400

    added config file

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -15,19 +15,33 @@ public class TestBase {
 
     @BeforeAll
     public static void setup() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = "chrome";
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
+        Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        boolean isRemote = Boolean.parseBoolean(System.getProperty("remote", "false"));
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
+        if (isRemote) {
+            Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
-        Configuration.browserCapabilities = capabilities;
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+
+            String browserVersion = System.getProperty("browserVersion", "128.0");
+
+            String browser = Configuration.browser;
+            if (browser.equalsIgnoreCase("chrome") || browser.equalsIgnoreCase("firefox")) {
+                capabilities.setCapability("version", browserVersion);
+            } else {
+                System.out.printf("Skipping version setting for unsupported browser: %s%n", browser);
+            }
+
+            Configuration.browserCapabilities = capabilities;
+        }
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
